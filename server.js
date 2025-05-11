e42b8fa4a161a830c98be12f247b375ebdfa4078
SHAIK FAYAZ <151929160+mywebsitebuilt@users.noreply.github.com>
2025-05-11 18:26:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -141,5 +141,5 @@ app.post("/api/login", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5010;
 app.listen(port, () => console.log(`Server running on port ${port}`));
